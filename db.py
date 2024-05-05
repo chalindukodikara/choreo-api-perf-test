54261048036b378760dcea75dce37b4e1d52702d
chalindkodikara <chalindu.18@cse.mrt.ac.lk>
2024-05-05 23:49:10 +0530

    Add average execution time calculator script

--- db.py
+++ db.py
@@ -6,23 +6,24 @@ import threading
 from kind import generate_component_kind, generate_build_kind, generate_deployment_kind, generate_project_kind
 
 connection_configs = {
-    "dbname": "postgres",
+    "dbname": "choreo_declarative_api_db",
     "user": "chalindu",
     "password": "Admin@1234",
     "host": "localhost",
     "port": "5432"
 }
+table_name = "desired_store"
 
 def clear_kind_table():
     try:
         conn = psycopg2.connect(**connection_configs)
         cursor = conn.cursor()
-        cursor.execute("TRUNCATE TABLE kind")
+        cursor.execute("TRUNCATE TABLE " + table_name)
         conn.commit()
         conn.close()
-        print("Kind table cleared successfully.")
+        print(table_name + " table cleared successfully.")
     except Exception as e:
-        print("Error clearing kind table:", e)
+        print("Error clearing " + table_name + " table:", e)
 
 def insert_data_with_threads(num_times, num_threads):
     def insert_thread():
@@ -50,13 +51,13 @@ def insert_multiple_kind_data_with_threads(num_times, num_threads):
             if conn.closed:
                 conn = psycopg2.connect(**connection_configs)
             if i % 4 == 0:
-                data = generate_project_kind()
+                data = generate_project_kind(table_name)
             elif i % 4 == 1:
-                data = generate_component_kind()
+                data = generate_component_kind(table_name)
             elif i % 4 == 2:
-                data = generate_build_kind()
+                data = generate_build_kind(table_name)
             else:
-                data = generate_deployment_kind()
+                data = generate_deployment_kind(table_name)
             insert_kind_at_once(data, conn)
         conn.close()
 
@@ -74,7 +75,7 @@ def insert_kind(data):
         conn = psycopg2.connect(**connection_configs)
         cursor = conn.cursor()
         cursor.execute(
-            sql.SQL("INSERT INTO kind (value) VALUES (%s)"), (json.dumps(data),)
+            sql.SQL("INSERT INTO " + table_name + " (payload) VALUES (%s)"), (json.dumps(data),)
         )
         conn.commit()
         conn.close()
@@ -85,7 +86,7 @@ def insert_kind_at_once(data, conn):
     try:
         cursor = conn.cursor()
         cursor.execute(
-            sql.SQL("INSERT INTO kind (value) VALUES (%s)"), (json.dumps(data),)
+            sql.SQL("INSERT INTO " + table_name + " (payload) VALUES (%s)"), (json.dumps(data),)
         )
         conn.commit()
     except Exception as e:
--- kind.py
+++ kind.py
@@ -3,16 +3,128 @@ import uuid
 
 fake = Faker()
 
-def generate_component_kind():
+def generate_component_kind(table_name):
+    if table_name == "desired_store":
+        return generate_component_kind_desired()
+    return generate_component_kind_actual()
+
+def generate_build_kind(table_name):
+    if table_name == "desired_store":
+        return generate_build_kind_desired()
+    return generate_build_kind_actual()
+
+def generate_deployment_kind(table_name):
+    if table_name == "desired_store":
+        return generate_deployment_kind_desired()
+    return generate_deployment_kind_actual()
+
+def generate_project_kind(table_name):
+    if table_name == "desired_store":
+        return generate_project_kind_desired()
+    return generate_project_kind_actual()
+
+def generate_component_kind_desired():
     component_name = fake.name()
     uuid_str = str(uuid.uuid4())
     fake_data = {
         "apiVersion": "core.choreo.dev/v1alpha1",
         "kind": "Component",
         "metadata": {
-            "name": component_name.lower().replace(' ', '_') + "_" + uuid_str + "_" + fake.word(),
-            "projectName": fake.word(),
-            "displayName": component_name
+            "name": component_name.lower().replace(' ', '_') + fake.word(),
+            "projectName": fake.name().lower().replace(' ', '_'),
+            "orgName": str(uuid.uuid4()),
+            "displayName": component_name,
+        },
+        "spec": {
+            "type": fake.random_element(["miApiService", "miCronjob", "miJob", "miWebhook", "byocCronjob", 
+                                          "byocJob", "byocWebApp", "byocService", "byocWebhook", 
+                                          "byocWebAppsDockerfileLess", "buildpackCronjob", "buildpackWebApp", 
+                                          "buildpackService", "buildpackTestRunner"]),
+            "source": {
+                "github": {
+                    "repository": "https://github.com/example/choreo-samples/tree/main/hello-world",
+                    "branch": "main",
+                    "path": "hello-world"
+                }
+            },
+            "build": {
+                "default": {
+                    "version": "1.x",
+                    "port": str(fake.random_int(min=1, max=65535))
+                }
+            }
+        }
+    }
+    return fake_data
+
+
+def generate_build_kind_desired():
+    fake_data = {
+        "apiVersion": "core.choreo.dev/v1alpha1",
+        "kind": "Build",
+        "metadata": {
+            "name": fake.name().lower().replace(' ', '_'),
+            "projectName": fake.name().lower().replace(' ', '_'),
+            "deploymentTrackId": str(uuid.uuid4()),
+            "orgName": str(uuid.uuid4()),
+            "componentName": fake.name().lower().replace(' ', '_'),
+            "labels": {
+                "environment": fake.random_element(["Development", "Staging", "Production"])
+            }
+        },
+        "spec": {
+            "revision": str(uuid.uuid4())
+        }
+    }
+    return fake_data
+
+
+
+def generate_deployment_kind_desired():
+    fake_data = {
+        "apiVersion": "core.choreo.dev/v1alpha1",
+        "kind": "Deployment",
+        "metadata": {
+            "name": str(uuid.uuid4()),
+            "projectName": fake.name().lower().replace(' ', '_'),
+            "deploymentTrackId": str(uuid.uuid4()),
+            "componentName": fake.name().lower().replace(' ', '_'),
+            "orgName": str(uuid.uuid4()),
+            "environment": fake.random_element(["Development", "Staging", "Production"])
+        },
+        "spec": {
+            "buildRef": str(uuid.uuid4())
+        }
+    }
+    return fake_data
+
+
+def generate_project_kind_desired():
+    fake_data = {
+        "apiVersion": "core.choreo.dev/v1alpha1",
+        "kind": "Project",
+        "metadata": {
+            "name": fake.name().lower().replace(' ', '_'),
+            "orgName": str(uuid.uuid4())
+        },
+        "spec": {
+            "'type": fake.random_element(["mono-repo", "multi-repo"]),
+            "owner": fake.name()
+        }
+    }
+    return fake_data
+
+def generate_component_kind_actual():
+    component_name = fake.name()
+    uuid_str = str(uuid.uuid4())
+    fake_data = {
+        "apiVersion": "core.choreo.dev/v1alpha1",
+        "kind": "Component",
+        "metadata": {
+            "name": component_name.lower().replace(' ', '_') + fake.word(),
+            "projectName": fake.name().lower().replace(' ', '_'),
+            "orgName": str(uuid.uuid4()),
+            "displayName": component_name,
         },
         "spec": {
             "type": fake.random_element(["miApiService", "miCronjob", "miJob", "miWebhook", "byocCronjob", 
@@ -33,173 +145,79 @@ def generate_component_kind():
                 }
             }
         },
-        "level1": {
-            "value1": fake.word(),
-            "level2": {
-                "value2": fake.word(),
-                "level3": {
-                    "value3": fake.word(),
-                    "level4": {
-                        "value4": fake.word(),
-                        "level5": {
-                            "value5": fake.word(),
-                            "level6": {
-                                "value6": fake.word(),
-                                "level7": {
-                                    "value7": fake.word(),
-                                    "level8": {
-                                        "value8": fake.word(),
-                                        "level9": {
-                                            "value9": fake.word(),
-                                            "level10": {
-                                                "value10": fake.word()
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+        "status": {
+            "state": fake.random_element(["Completed", "Pending", "Failed", "Running"]),
+            "completedAt": fake.date(),
         }
     }
     return fake_data
 
 
-def generate_build_kind():
+def generate_build_kind_actual():
     fake_data = {
         "apiVersion": "core.choreo.dev/v1alpha1",
         "kind": "Build",
         "metadata": {
             "name": fake.name().lower().replace(' ', '_'),
-            "projectName": fake.word(),
+            "projectName": fake.name().lower().replace(' ', '_'),
             "deploymentTrackId": str(uuid.uuid4()),
-            "componentName": fake.name().lower().replace(' ', '_')
+            "orgName": str(uuid.uuid4()),
+            "componentName": fake.name().lower().replace(' ', '_'),
+            "labels": {
+                "environment": fake.random_element(["Development", "Staging", "Production"])
+            }
         },
         "spec": {
             "revision": str(uuid.uuid4())
         },
-        "level1": {
-            "value1": fake.word(),
-            "level2": {
-                "value2": fake.word(),
-                "level3": {
-                    "value3": fake.word(),
-                    "level4": {
-                        "value4": fake.word(),
-                        "level5": {
-                            "value5": fake.word(),
-                            "level6": {
-                                "value6": fake.word(),
-                                "level7": {
-                                    "value7": fake.word(),
-                                    "level8": {
-                                        "value8": fake.word(),
-                                        "level9": {
-                                            "value9": fake.word(),
-                                            "level10": {
-                                                "value10": fake.word()
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+        "status": {
+            "state": fake.random_element(["Completed", "Pending", "Failed", "Running"]),
+            "completedAt": fake.date(),
         }
     }
     return fake_data
 
 
 
-def generate_deployment_kind():
+def generate_deployment_kind_actual():
     fake_data = {
         "apiVersion": "core.choreo.dev/v1alpha1",
         "kind": "Deployment",
         "metadata": {
             "name": str(uuid.uuid4()),
-            "projectName": fake.word(),
+            "projectName": fake.name().lower().replace(' ', '_'),
             "deploymentTrackId": str(uuid.uuid4()),
             "componentName": fake.name().lower().replace(' ', '_'),
+            "orgName": str(uuid.uuid4()),
             "environment": fake.random_element(["Development", "Staging", "Production"])
         },
         "spec": {
             "buildRef": str(uuid.uuid4())
         },
-        "level1": {
-            "value1": fake.word(),
-            "level2": {
-                "value2": fake.word(),
-                "level3": {
-                    "value3": fake.word(),
-                    "level4": {
-                        "value4": fake.word(),
-                        "level5": {
-                            "value5": fake.word(),
-                            "level6": {
-                                "value6": fake.word(),
-                                "level7": {
-                                    "value7": fake.word(),
-                                    "level8": {
-                                        "value8": fake.word(),
-                                        "level9": {
-                                            "value9": fake.word(),
-                                            "level10": {
-                                                "value10": fake.word()
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+        "status": {
+            "state": fake.random_element(["Completed", "Pending", "Failed", "Running"]),
+            "completedAt": fake.date(),
+            "runs": fake.random_int(min=1, max=1000)
         }
     }
     return fake_data
 
 
-def generate_project_kind():
+def generate_project_kind_actual():
     fake_data = {
         "apiVersion": "core.choreo.dev/v1alpha1",
         "kind": "Project",
         "metadata": {
-            "name": fake.name(),
-            "orgName": fake.word()
+            "name": fake.name().lower().replace(' ', '_'),
+            "orgName": str(uuid.uuid4())
         },
-        "level1": {
-            "value1": fake.word(),
-            "level2": {
-                "value2": fake.word(),
-                "level3": {
-                    "value3": fake.word(),
-                    "level4": {
-                        "value4": fake.word(),
-                        "level5": {
-                            "value5": fake.word(),
-                            "level6": {
-                                "value6": fake.word(),
-                                "level7": {
-                                    "value7": fake.word(),
-                                    "level8": {
-                                        "value8": fake.word(),
-                                        "level9": {
-                                            "value9": fake.word(),
-                                            "level10": {
-                                                "value10": fake.word()
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+         "spec": {
+            "'type": fake.random_element(["mono-repo", "multi-repo"]),
+            "owner": fake.name()
+        },
+        "status": {
+            "state": fake.random_element(["Completed", "Pending", "Failed", "Running"]),
+            "completedAt": fake.date()
         }
     }
     return fake_data
